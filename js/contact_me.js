89cb5d83e19d319d3c156b18df2b4222a4aaabc2
David Miller <hello@davidmiller.io>
2017-03-31 22:42:47 -0400

    dev environment

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./bin/contact_me.php",
+                url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
