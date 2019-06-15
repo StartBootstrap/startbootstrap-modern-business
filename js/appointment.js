c964a00a2877dae10eb18b2b93653e88623b3564
Chelsia Liu <chelsia.liu3@gmail.com>
2019-06-15 00:57:11 -0600

    Added in files needed for Appointment Form functionality

--- js/appointment.js
+++ js/appointment.js
@@ -1,6 +1,6 @@
  $(function() {
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $("#appointmentForm input,#appointmentForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
@@ -11,7 +11,9 @@
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      var dayofweek = $("textarea#dayofweek").val();
+      var timeofday = $("textarea#timeofday").val();
+      var reasonforappt = $("textarea#reasonforappt").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -20,13 +22,15 @@
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "././mail/appointment.php",
         type: "POST",
         data: {
           name: name,
           phone: phone,
           email: email,
-          message: message
+          dayofweek: dayofweek,
+          timeofday: timeofday,
+          reasonforappt: reasonforappt
         },
         cache: false,
         success: function() {
@@ -35,21 +39,21 @@
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Your appointment request has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#appointmentForm').trigger("reset");
         },
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("CONT Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("APPT Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#appointmentForm').trigger("reset");
         },
         complete: function() {
           setTimeout(function() {
