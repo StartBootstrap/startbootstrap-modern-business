3e25d0672a0a5efbfea9a18dfb130be138520c5c
Chelsia Liu <chelsia.liu3@gmail.com>
2019-06-21 12:53:40 -0600

    Editted appointment form email format

--- mail/appointment.php
+++ mail/appointment.php
@@ -22,7 +22,7 @@ $reasonforappt = strip_tags(htmlspecialchars($_POST['reasonforappt']));
 // Create the email and send the message
 $to = 'smileymolar@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Appointment Form:  $name";
-$email_body = "You have received a new message from your website appointment form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\Preferred Day of Week:\n$dayofweek\n\Preferred Time of Day:\n$timeofday\n\Reason for Appointment:\n$reasonforappt";
+$email_body = "You have received a new message from your website appointment form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nPreferred Day of Week: $dayofweek\n\nPreferred Time of Day: $timeofday\n\nReason for Appointment: $reasonforappt";
 $headers = "From: noreply@smileymolar.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
