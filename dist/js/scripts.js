95bfb21934c55da424ca7fb7f4daf6d97a7d5b5a
David Miller <hello@davidmiller.io>
2021-04-28 13:45:21 -0400

    Bootstrap 4.6.0 and dev environment update

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Modern Business v4.3.0 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
