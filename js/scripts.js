867d6baecf6ee2366c8554dc68597a8388155e11
David Miller <hello@davidmiller.io>
2022-03-22 16:36:01 -0400

    dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Modern Business v5.0.5 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
